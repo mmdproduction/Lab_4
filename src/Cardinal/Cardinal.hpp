d8157f211e0c5fee3845d3e5401f21a1547c3ae5
Матвей Александров <mmdproduction8@gmail.com>
2026-05-27 21:19:32 +0300

    feat: LazySequence structure and some Constructors

---
+++ src/Cardinal/Cardinal.hpp
@@ -0,0 +1,7 @@
+#include "Optional.hpp"
+
+
+class Cardinal{
+    private:
+    
+}
---
+++ src/LazySequence/LazySequence.hpp
@@ -0,0 +1,36 @@
+#include "Generator.hpp"
+#include "Exception.hpp"
+#include "Cardinal.hpp"
+#include "Sequence.hpp"
+#include<initializer_list>
+#include<functional>
+
+
+
+template <typename T>
+class LazySequence{
+    private:
+
+    IGenerator<T>* generator_;
+    ArraySequence<T> materialized_;
+
+    bool is_infinite_;
+
+    size_t materialized_count_;
+
+
+    public:
+
+    LazySequence() : is_infinite_(false){}
+
+    LazySequence(std::initializer_list list): materialized_(ArraySequence(list)){}
+
+    LazySequence(T* items, size_t count): materialized_(ArraySequence(items, count)){}
+
+    LazySequence(std::function<T(Sequence<T>*)> rule, Sequence<T>* seed): is_infinite_(true){
+        //TODO reccurent infinite LazySequence
+        }
+    
+
+    
+};
