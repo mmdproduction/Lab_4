41fc0a3cea718356120bf339599c4954b5bbaf89
Матвей Александров <mmdproduction8@gmail.com>
2026-05-27 20:59:10 +0300

    feat: tests; fix: CMakeLists

--- src/Optional/Optional.hpp
+++ src/Optional/Optional.hpp
@@ -71,5 +71,5 @@ class Optional{
  
     ~Optional() { reset(); }
 
-}
+};
 
---
+++ tests/OptionalTest.cpp
@@ -0,0 +1,52 @@
+#include <gtest/gtest.h>
+#include "Optional.hpp"
+#include "Exception.hpp"
+
+TEST(Optional, NoneHasNoValue) {
+    auto opt = Optional<int>::none();
+    EXPECT_FALSE(opt.has_value());
+}
+ 
+TEST(Optional, SomeHasValue) {
+    auto opt = Optional<int>::some(42);
+    EXPECT_TRUE(opt.has_value());
+    EXPECT_EQ(opt.value(), 42);
+}
+ 
+TEST(Optional, ValueOnNoneThrows) {
+    auto opt = Optional<int>::none();
+    EXPECT_THROW(opt.value(), OptionalHasNotValue);
+}
+ 
+TEST(Optional, CopyOfSomeHasValue) {
+    auto opt = Optional<int>::some(7);
+    auto copy = opt;
+    EXPECT_TRUE(copy.has_value());
+    EXPECT_EQ(copy.value(), 7);
+}
+ 
+TEST(Optional, CopyOfNoneHasNoValue) {
+    auto opt = Optional<int>::none();
+    auto copy = opt;
+    EXPECT_FALSE(copy.has_value());
+}
+ 
+TEST(Optional, AssignSomeToNone) {
+    auto opt = Optional<int>::none();
+    opt = Optional<int>::some(99);
+    EXPECT_TRUE(opt.has_value());
+    EXPECT_EQ(opt.value(), 99);
+}
+ 
+TEST(Optional, AssignNoneToSome) {
+    auto opt = Optional<int>::some(5);
+    opt = Optional<int>::none();
+    EXPECT_FALSE(opt.has_value());
+}
+ 
+TEST(Optional, WorksWithNonTrivialType) {
+    auto opt = Optional<std::string>::some("hello");
+    EXPECT_TRUE(opt.has_value());
+    EXPECT_EQ(opt.value(), "hello");
+}
+
